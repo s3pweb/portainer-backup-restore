af24541f25162b66a88b98a4c8cd2e6defb1bf90
Mualig <mualig@gmail.com>
2018-06-25 16:58:56 +0200

    feat: log to portainer and retrieve stackfiles

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 const log = require('./lib/logger/logger')('Main')
+const portainer = require('./lib/portainer/portainer.utils')
 
-log.info('Hello World !')
+async function main () {
+  log.info('Starting app')
+
+  let jwt = await portainer.auth()
+  log.info(`Logged in`)
+
+  let stacks = await portainer.getStacks(jwt)
+  log.info(`Found ${stacks.length} stack(s).`)
+
+  for (let stack of stacks) {
+    let stackFile = await portainer.getStackFile(jwt, stack.Id)
+    log.info({stackFile: stackFile}, `Found stack file for id  ${stack.Id} stack(s).`)
+  }
+}
+
+main()
+  .catch((err) => {
+    log.error({err: err}, `Error`)
+  })
