208b3ab6789f24df379e038fd9a7b3ea60fac04a
Mualig <mualig@gmail.com>
2018-07-04 11:49:05 +0200

    feat: added yargs

--- app.js
+++ app.js
@@ -1,20 +1,60 @@
+const yargs = require('yargs')
+
 const log = require('./lib/logger/logger')('Main')
-const portainer = require('./lib/portainer/portainer.utils')
 const stacks = require('./lib/stacks/stacks.utils')
 
+const argv = yargs
+  .command('backup', 'Back up stacks from portainer')
+  .command('update', 'Update the backed up  stacks')
+  .command('create', 'Remove then create the backed up stacks')
+  .options({
+    'u': {
+      demand: false,
+      alias: 'url',
+      describe: 'Portainer\'s URL',
+      string: true
+    },
+    'l': {
+      demand: false,
+      alias: 'login',
+      describe: 'User\'s login to use',
+      string: true
+    },
+    'p': {
+      demand: false,
+      alias: 'password',
+      describe: 'User\'s password',
+      string: true
+    }
+  })
+  .implies('url', 'login')
+  .implies('login', 'password')
+  .implies('login', 'url')
+  .implies('password', 'login')
+  .implies('password', 'url')
+  .help()
+  .alias('help', 'h')
+  .locale('en')
+  .argv
+
 async function main () {
-  log.debug('Starting app')
+  log.debug({yargs: argv}, 'Starting app')
 
   // Log in and get JWT token from portainer
-  let jwt = await portainer.login()
-  log.info({jwt: jwt}, `Logged in`)
+  let jwt = await stacks.login(argv.url, argv.login, argv.password)
 
-  // Get all stacks from portainer and save them to a json file
-  await stacks.backupStacks(jwt)
-
-  // await stacks.updateStacks(jwt)
-
-  await stacks.removeAndCreateStacks(jwt)
+  if (argv._[0] === 'backup') {
+    // Get all stacks from portainer and save them to a json file
+    await stacks.backupStacks(jwt, argv.url)
+  } else if (argv._[0] === 'update') {
+    // Read the backup file then update all the stacks
+    await stacks.updateStacks(jwt, argv.url)
+  } else if (argv._[0] === 'create') {
+    // Read the backup file then re-create all the stacks
+    await stacks.removeAndCreateStacks(jwt, argv.url)
+  } else {
+    log.warn(`Unknown command`)
+  }
 }
 
 main()
