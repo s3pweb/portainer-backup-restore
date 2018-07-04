05f217a7638e1bf8285b9fc0c7185b3550643946
Mualig <mualig@gmail.com>
2018-07-04 11:55:24 +0200

    test: fixed tests broken by yargs integration

--- test/portainer.test.js
+++ test/portainer.test.js
@@ -1,19 +1,21 @@
 import test from 'ava'
 import { before, after } from './utils'
 
+const config = require('config')
+
 const portainer = require('../lib/portainer/portainer.utils')
 
 test.before(before)
 
 test('Log user', async t => {
-  let jwt = await portainer.login()
+  let jwt = await portainer.login(config.portainer.url, config.portainer.login, config.portainer.password)
 
   t.true(!!jwt, 'JWT does not exist')
   t.is(jwt, 'jwt_token', 'JWT token is not jwt_token')
 })
 
 test('Get all stacks', async t => {
-  let data = await portainer.getStacks(undefined)
+  let data = await portainer.getStacks(undefined, config.portainer.url)
 
   t.true(!!data, 'Data exist')
   t.is(data.length, 2, 'Array size is not 2')
@@ -22,7 +24,7 @@ test('Get all stacks', async t => {
 })
 
 test('Get stack file', async t => {
-  let data = await portainer.getStackFile(undefined, 'id')
+  let data = await portainer.getStackFile(undefined, config.portainer.url, 'id')
 
   t.true(!!data, 'Data does not exist')
   t.true(!!data.StackFileContent, 'StackFileContent does not exist')
--- test/stacks.test.js
+++ test/stacks.test.js
@@ -12,12 +12,12 @@ let filename = config.backupFile
 test.before(before)
 
 test('Backup stacks to FS', async t => {
-  let jwt = await portainer.login()
+  let jwt = await portainer.login(config.portainer.url, config.portainer.login, config.portainer.password)
 
   t.true(!!jwt, 'JWT does not exist')
   t.is(jwt, 'jwt_token', 'JWT token is not jwt_token')
 
-  await stacks.backupStacks(jwt)
+  await stacks.backupStacks(jwt, config.portainer.url)
 
   let data = await bf.readFromBackupFile(filename)
   let backupStacks = JSON.parse(data)
